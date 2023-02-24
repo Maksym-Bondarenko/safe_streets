5265034273afaa2144543b9d29c3e7cb7ee98103
Maksym Bondarenko <ga92nib@mytum.de>
2023-02-24 17:57:58 +0100

    added API-placeholders for android, ios, web

--- lib/home/start_page.dart
+++ lib/home/start_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class StartPage extends StatefulWidget {
   const StartPage({super.key});
@@ -8,10 +9,37 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPage extends State<StartPage> {
-  int _test = 42;
+
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
   
   @override
   Widget build(BuildContext context) {
+    // return MaterialApp(
+    //   theme: ThemeData(
+    //     useMaterial3: true,
+    //     colorSchemeSeed: Colors.green[700],
+    //   ),
+    //   home: Scaffold(
+    //     appBar: AppBar(
+    //       title: const Text('Safe Streets Maps'),
+    //       elevation: 2,
+    //     ),
+    //     body: GoogleMap(
+    //       onMapCreated: _onMapCreated,
+    //       initialCameraPosition: CameraPosition(
+    //         target: _center,
+    //         zoom: 11.0,
+    //       ),
+    //     ),
+    //   ),
+    // );
+
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(20),
