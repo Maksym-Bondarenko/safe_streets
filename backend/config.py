df46005cbc75a18534ee4168fca549c779d5b728
Андрей Чернов <chernovandrey@MBP-Andrej.fritz.box>
2023-06-06 22:54:12 +0200

    add safe places

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 from google.cloud.sql.connector import IPTypes
-import os
-instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]
-db_user = os.environ["DB_USER"]
-db_pass = os.environ["DB_PASS"]
-db_name = os.environ["DB_NAME"]
+import config_secret
+instance_connection_name = config_secret.instance_connection_name
+db_user = "postgres"
+db_pass = config_secret.db_pass
+db_name = "postgres"
 ip_type = IPTypes.PUBLIC
