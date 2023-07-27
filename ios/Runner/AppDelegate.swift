237e5b28a3202f7356c01454b25957616347fa6f
Ekaterina Erofeeva <ekaterina.erofeeva@sap.com>
2023-07-27 17:02:26 +0200

    Fix bug:Conditional cast from 'AppDelegate' to 'UNUserNotificationCenterDelegate' always succeeds

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -18,7 +18,7 @@ import flutter_local_notifications
     GeneratedPluginRegistrant.register(with: self)
 
     if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+      UNUserNotificationCenter.current().delegate = self
     }
 
 //  TODO: Add your Google Maps API key here
