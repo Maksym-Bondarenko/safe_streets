92c88bb59e35d88776c32d5e039b52b02ba52f20
Андрей Чернов <chernov.andrey.998@gmail.com>
2023-08-16 19:31:08 +0200

    change commit

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 from google.cloud.sql.connector import IPTypes
-import config_secret
+# import config_secret
 # instance_connection_name = config_secret.instance_connection_name
 db_user = "chernovandrey"
 # db_pass = config_secret.db_pass
