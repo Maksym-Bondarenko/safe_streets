e85467aeaff4547816a4e760738500d05c7fe90b
Eduard Munkhart <eduard.munkhart@gmail.com>
2024-05-10 15:06:17 +0200

    Refactor utils

---
+++ lib/utils/assets.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'dart:ui' as ui;
+
+import 'package:flutter/services.dart';
+
+// change default google-marker-icon to custom one
+Future<Uint8List?> getBytesFromAsset(String path, int width) async {
+  ByteData data = await rootBundle.load(path);
+  ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+      targetWidth: width);
+  ui.FrameInfo fi = await codec.getNextFrame();
+  return (await fi.image.toByteData(format: ui.ImageByteFormat.png))
+      ?.buffer
+      .asUint8List();
+}
