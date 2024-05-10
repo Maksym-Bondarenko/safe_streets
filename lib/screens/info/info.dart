56074e00cecc8990b1dcfac48963de6cb1b8b176
Eduard Munkhart <eduard.munkhart@gmail.com>
2024-05-10 16:44:05 +0200

    Refactor info screens

---
+++ lib/screens/info/info.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+
+import 'package:safe_streets/router.dart';
+
+// Info page after authentication, containing dashboard with main information,
+// settings, and navigation to further pages
+class InfoScreen extends StatelessWidget {
+  const InfoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Title
+            const Padding(
+              padding: EdgeInsets.only(bottom: 80.0),
+              child: Text(
+                "From\nSafe Streets\nto safe cities,\ncountries, and\nthe world!",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            const Divider(),
+            // Forum card
+            IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: ForumCard(
+                      onTap: () => const ForumRoute().push(context),
+                    ),
+                  ),
+                  const SizedBox(width: 5),
+                  Expanded(
+                    flex: 1,
+                    child: SupportCard(
+                      onTap: () => const SupportRoute().push(context),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Rank-based Map button
+            ElevatedButton(
+              onPressed: () => const DDSMapRoute().push(context),
+              child: const Text(
+                "Rank-based Map",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// Forum card widget
+class ForumCard extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const ForumCard({
+    super.key,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 12,
+      color: Colors.white,
+      borderOnForeground: true,
+      clipBehavior: Clip.hardEdge,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+        side: const BorderSide(
+          color: Colors.blue,
+          width: 2.0,
+          style: BorderStyle.solid,
+        ),
+      ),
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        onTap: onTap,
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(4.0),
+              child: Text(
+                "Forum",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Text(
+              "Join area chats and discuss points, meet community mates and many more!",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// Support card widget
+class SupportCard extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const SupportCard({
+    super.key,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 12,
+      color: Colors.white,
+      borderOnForeground: true,
+      clipBehavior: Clip.hardEdge,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+        side: const BorderSide(
+          color: Colors.blue,
+          width: 2.0,
+          style: BorderStyle.solid,
+        ),
+      ),
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        onTap: onTap,
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(4.0),
+              child: Text(
+                "Support",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Text(
+              "Search for official emergency contacts in your country.",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/info/support.dart
@@ -0,0 +1,259 @@
+import 'package:flutter/material.dart';
+
+class SupportScreen extends StatelessWidget {
+  const SupportScreen({super.key});
+
+  final String policeNumber = "110";
+  final String fireServiceNumber = "112";
+  final String medicalServiceNumber = "112";
+  final String minorCrime = "0800 6 888 000";
+  final String medicalServices = "116 117";
+  final String cardCancellation = "116 116";
+  final String generalHelpline = "080011101 OR 1108001110122";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Support')),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: const BorderSide(
+                  color: Colors.blue,
+                  width: 2,
+                  style: BorderStyle.solid,
+                ),
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Column(children: [
+                  Text(
+                    "Main Emergency Information".toUpperCase(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      RichText(
+                        text: const TextSpan(
+                          style: TextStyle(color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: 'Police: ',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            TextSpan(
+                              text: policeNumber,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      RichText(
+                        text: const TextSpan(
+                          style: TextStyle(color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: 'Fire Service: ',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            TextSpan(
+                              text: fireServiceNumber,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      RichText(
+                        text: const TextSpan(
+                          style: TextStyle(color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: 'Medical Service: ',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            TextSpan(
+                              text: medicalServiceNumber,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ]),
+              ),
+            ),
+            const SizedBox(height: 24),
+            const Divider(),
+            const SizedBox(height: 24),
+            Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: const BorderSide(
+                  color: Colors.blue,
+                  width: 2,
+                  style: BorderStyle.solid,
+                ),
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text(
+                      "Minor Emergency Information".toUpperCase(),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      children: [
+                        RichText(
+                          text: const TextSpan(
+                            style: TextStyle(color: Colors.black),
+                            children: [
+                              TextSpan(
+                                text: 'Minor crime: ',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                ),
+                              ),
+                              TextSpan(
+                                text: minorCrime,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: [
+                        RichText(
+                          text: const TextSpan(
+                            style: TextStyle(color: Colors.black),
+                            children: [
+                              TextSpan(
+                                text: 'Medical services: ',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                ),
+                              ),
+                              TextSpan(
+                                text: medicalServices,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: [
+                        RichText(
+                          text: const TextSpan(
+                            style: TextStyle(color: Colors.black),
+                            children: [
+                              TextSpan(
+                                text: 'Card cancellation in case of lost or theft: ',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                ),
+                              ),
+                              TextSpan(
+                                text: cardCancellation,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: [
+                        RichText(
+                          text: const TextSpan(
+                            style: TextStyle(color: Colors.black),
+                            children: [
+                              TextSpan(
+                                text: 'General helpline: ',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                ),
+                              ),
+                              TextSpan(
+                                text: generalHelpline,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
