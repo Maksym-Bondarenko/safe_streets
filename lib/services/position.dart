99657427511356089f0a80624e863ab5b25840d8
Eduard Munkhart <eduard.munkhart@gmail.com>
2024-05-29 13:18:56 +0200

    Fix position provider

--- lib/services/position.dart
+++ lib/services/position.dart
@@ -5,5 +5,13 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'position.g.dart';
 
+const _minLocationTreshholdInMeters = 10;
+const _locationSettings = LocationSettings(
+  accuracy: LocationAccuracy.high,
+  distanceFilter: _minLocationTreshholdInMeters,
+);
+
 @riverpod
-Stream<Position> position(PositionRef ref) => Geolocator.getPositionStream();
+Stream<Position> position(PositionRef ref) async* {
+  yield* Geolocator.getPositionStream(locationSettings: _locationSettings);
+}
