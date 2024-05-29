f5f3eda0d3831dc3e51f32bca78b196ac8f46641
Eduard Munkhart <eduard.munkhart@gmail.com>
2024-05-29 13:33:56 +0200

    Add map search provider

--- lib/screens/map/providers/map_search.dart
+++ lib/screens/map/providers/map_search.dart
@@ -1,22 +1,55 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import 'package:safe_streets/models/places.dart';
+import 'package:safe_streets/screens/map/providers/map_controller.dart';
+import 'package:safe_streets/screens/map/providers/selected_place.dart';
 import 'package:safe_streets/services/places.dart';
+import 'package:safe_streets/utils/map.dart';
 
 part 'map_search.g.dart';
 
-@riverpod
-class MapSearchQuery extends _$MapSearchQuery {
-  @override
-  String build() => '';
+const _defaultMaxNumOfTextSearchResults = 10;
 
-  void set(String query) {
-    state = query;
+@riverpod
+class MapSearchResults extends _$MapSearchResults {
+  @override
+  FutureOr<List<Place>> build() {
+    return [];
+  }
+
+  Future<void> searchByQuery(
+    String query, {
+    int? pageSize,
+  }) async {
+    final mapControllerNotifier = ref.read(mapControllerProvider.notifier);
+    final selectedPlaceNotifier = ref.read(selectedPlaceProvider.notifier);
+    final results = await ref.read(placesServiceProvider.notifier).searchByText(
+          query,
+          pageSize: pageSize ?? _defaultMaxNumOfTextSearchResults,
+        );
+    if (results.length == 1) {
+      selectedPlaceNotifier.set(results.first);
+      mapControllerNotifier.centerOnSelectedPlace();
+    } else if (results.length > 1) {
+      final bounds = getLatLngBoundsOfPlaces(results);
+      mapControllerNotifier.centerOnBounds(bounds);
+      selectedPlaceNotifier.clear();
+    }
+    state = AsyncData(results);
+  }
+
+  void set(Place place) {
+    state = AsyncData([place]);
+  }
+
+  void clear() {
+    if (state.hasValue && state.value!.isNotEmpty) {
+      ref.invalidateSelf();
+    }
   }
 }
 
-@riverpod
-Future<List<Suggestion>> mapSearchAutocomplete(MapSearchAutocompleteRef ref) async {
-  final query = ref.watch(mapSearchQueryProvider);
+@Riverpod(keepAlive: true)
+Future<List<Suggestion>> mapSearchAutocomplete(MapSearchAutocompleteRef ref, String query) async {
   return await ref.read(placesServiceProvider.notifier).autocomplete(query);
 }
